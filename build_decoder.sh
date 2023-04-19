30892b6e94123f82e7b3f82f2ec2266c6f5a7c01
“duanyuanyuan123” <2469241792@qq.com>
2023-04-19 20:58:53 +0800

    sh

--- build_decoder.sh
+++ build_decoder.sh
@@ -3,7 +3,7 @@ rm -r dist
 mkdir -p dist
 cd ../ffmpeg
 echo "emconfigure"
-emconfigure ./configure --cc="emcc" --cxx="em++" --ar="emar" --ranlib="emranlib" --prefix=$(pwd)/../WasmVideoPlayer/dist --enable-cross-compile --target-os=none \
+emconfigure ./configure --cc="emcc" --cxx="em++" --ar="emar" --ranlib="emranlib" --prefix=$(pwd)/../wasmh264player/dist --enable-cross-compile --target-os=none \
         --arch=x86_32 --cpu=generic --enable-gpl --enable-version3 --disable-avdevice --disable-swresample --disable-postproc --disable-avfilter \
         --disable-programs --disable-logging --disable-everything --enable-avformat --enable-decoder=hevc --enable-decoder=h264 --enable-decoder=aac \
         --disable-ffplay --disable-ffprobe --disable-asm --disable-doc --disable-devices --disable-network --disable-hwaccels \
